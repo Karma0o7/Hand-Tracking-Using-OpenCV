ebc4fb9628439e3c302b678b5e1e4631e609547f
Karma0o7 <23173amitkanand@gmail.com>
2023-01-14 11:31:20 +0530

    Added Description

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,3 +1,11 @@
+"""
+Hand Tracking Module
+This is hand tracking module which can be used to track the hand and find the position of the landmarks 
+and draw the landmarks on the hand
+Made Using OpenCV and Mediapipe 
+"""
+
+
 import cv2
 import mediapipe as mp
 import time
@@ -15,6 +23,12 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils
  
     def findHands(self, img, draw=True):
+        """
+        This function finds the hands in the image and draws the landmarks on the hand
+        :param img: Image in which the hand is to be detected
+        :param draw: Boolean value to draw the landmarks on the hand
+        :return: Image with the landmarks drawn on the hand
+        """
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
  
@@ -26,6 +40,14 @@ class handDetector():
         return img
  
     def findPosition(self, img, handNo=0, draw=True, handPoint=None):
+        """
+        This function finds the position of the landmarks in the image
+        :param img: Image in which the hand is to be detected
+        :param handNo: Hand on which the landmarks are to be drawn
+        :param draw: Boolean value to draw the landmarks on the hand
+        :param handPoint: Point of the hand to be detected
+        :return: List of the landmarks
+        """
         lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
@@ -34,7 +56,7 @@ class handDetector():
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 lmList.append([id, cx, cy])
                 if draw and id == handPoint:
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 8, (255, 0, 255), cv2.FILLED)
         return lmList
 
  
